91ee2dc8baebadcfe66956d6245cb3561e072a7b
Daniel Askan Filsoof Mikkelsen <dam@mjolner.dk>
2024-11-11 15:17:42 +0100

    fix: update image paths for sheet music assets

--- bandhub/src/features/variables.ts
+++ bandhub/src/features/variables.ts
@@ -2,16 +2,16 @@ export const sheetMusic = [
   {
     id: 1,
     name: "This Land Is Your Land",
-    image: "/src/assets/sheets/this land is your land.jpg",
+    image: "../public/sheets/this land is your land.jpg",
   },
   {
     id: 2,
     name: "Old Rosin the Beau",
-    image: "/src/assets/sheets/old rosin the beau.jpg",
+    image: "../public/sheets/old rosin the beau.jpg",
   },
   {
     id: 3,
     name: "Skipper Klements morgensang",
-    image: "/src/assets/sheets/klementsMorgensang.jpg",
+    image: "../public/sheets/klementsMorgensang.jpg",
   },
 ];
